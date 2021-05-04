8e6321170f80e84f99db4ce92835b65cd875069c
zlksnk <zlksnkwork@gmail.com>
2021-05-05 00:24:58 +0530

    execute `up` command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -57,3 +57,36 @@ begin
     source $AUTOJUMP_PATH
   end
 end
+
+function up
+  # set args (getopt -s sh abc: $argv); or help_exit
+  # echo $argv[1]
+  set -l dir_to_be_child $argv[1]
+  # intentionally written this way because I'm learning syntax
+  # https://stackoverflow.com/a/29177261/7683365
+  if [ "$dir_to_be_child" = "" ]
+    cd ..
+  else
+    cd -
+    set -l alt_dir $PWD
+    cd -
+    set -l curr_dir $PWD
+    set -l is_found 0
+    while [ "$PWD" != "/" ]; and test $is_found -eq 0
+      cd ..
+      set -l res (find $dir_to_be_child -maxdepth 0 -type d | wc -l)
+      if test $res -eq 1
+        set is_found 1
+      end
+    end
+    if [ "$PWD" = "/" ]
+      cd $alt_dir
+      cd $curr_dir
+      echo "Couldn't find $dir_to_be_child"
+    else
+      set -l found_dir $PWD
+      cd $curr_dir
+      cd $found_dir
+    end
+  end
+end
