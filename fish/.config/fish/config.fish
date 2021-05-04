57d776310025597e0195dd0a9145ed08bd49f53d
zlksnk <zlksnkwork@gmail.com>
2021-05-05 01:07:25 +0530

    error when not found

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -71,6 +71,7 @@ function up
     set -l alt_dir $PWD
     cd -
     set -l curr_dir $PWD
+
     set -l is_found 0
     while [ "$PWD" != "/" ]; and test $is_found -eq 0
       cd ..
@@ -80,14 +81,16 @@ function up
         set is_found 1
       end
     end
+
     if [ "$PWD" = "/" ]
       cd $alt_dir
       cd $curr_dir
       echo "Couldn't find $dir_to_be_child"
-    else
-      set -l found_dir $PWD
-      cd $curr_dir
-      cd $found_dir
+      return 1
     end
+
+    set -l found_dir $PWD
+    cd $curr_dir
+    cd $found_dir
   end
 end
