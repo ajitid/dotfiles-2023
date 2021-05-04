505b9298478814626b4296b9b4849db030f26940
zlksnk <zlksnkwork@gmail.com>
2021-05-05 01:03:26 +0530

    silent errors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -74,7 +74,8 @@ function up
     set -l is_found 0
     while [ "$PWD" != "/" ]; and test $is_found -eq 0
       cd ..
-      set -l res (find $dir_to_be_child -maxdepth 0 -type d | wc -l)
+      # https://www.unix.com/shell-programming-and-scripting/46716-execute-command-silently-quietly-within-shell.html
+      set -l res (find $dir_to_be_child -maxdepth 0 -type d 2>/dev/null | wc -l)
       if test $res -eq 1
         set is_found 1
       end
