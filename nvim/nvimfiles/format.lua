d7891d1a587f8040572eedff75dff90b2e8016ce
zlksnk <zlksnkwork@gmail.com>
2021-05-10 20:40:46 +0530

    add prettier info

--- nvim/nvimfiles/format.lua
+++ nvim/nvimfiles/format.lua
@@ -3,6 +3,11 @@
 -- also seems like efm-langserver is better for formatting:
 -- - https://github.com/fsouza/prettierd#editor-integration
 -- - https://old.reddit.com/r/neovim/comments/jvisg5/lets_talk_formatting_again/
+
+-- for prettier
+-- exe = "npx prettier",
+-- args = {vim.api.nvim_buf_get_name(0), "--stdin"},
+
 local prettier = function()
 	return {
 		-- install from npm -> @fsouza/prettierd
