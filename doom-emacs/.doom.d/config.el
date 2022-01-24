76bd125bb6964bd7c0f70e0147936304b78c73db
ajit <zlksnkwork@gmail.com>
2022-01-24 12:56:59 +0530

    comment

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -67,6 +67,7 @@
 
 ;; to rename a TS/JS file, there's code for that https://github.com/emacs-lsp/lsp-mode/commit/1d9da9f24fd477faa2a38b369842a27fe5bda160
 ;; only thing being to rename, it copies fileA to fileB, and doesn't deletes fileB by itself, which I should code it in
+;; also mentioned here https://discord.com/channels/789885435026604033/789890622424219658/931959632937746522
 
 ;; don't format with LSP. By default if LSP is avialable, formatter (like prettier) isn't used
 ;; from https://docs.doomemacs.org/latest/modules/editor/format/
