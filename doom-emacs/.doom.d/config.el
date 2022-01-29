05315f70e98ef0e675d444b29638176e303ba373
ajit <zlksnkwork@gmail.com>
2022-01-30 01:01:02 +0530

    swapping places

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -21,12 +21,18 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "iosevka term ss08" :size 18)
+      doom-variable-pitch-font ()
+      doom-unicode-font ()
+      doom-big-font (font-spec :family "iosevka term ss08" :size 22))
+
+(setq default-text-properties '(line-spacing 0.1 line-height 1.1))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-miramare)
-;; miramare, monokai-ristretto, flatwhite, challenger-deep
+;; Other themes I like: miramare, monokai-ristretto, flatwhite, challenger-deep
 
 (setq evil-normal-state-cursor '(box "#ccc")
       evil-insert-state-cursor '(bar "#ccc")
@@ -57,12 +63,16 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq doom-font (font-spec :family "iosevka term ss08" :size 18)
-      doom-variable-pitch-font ()
-      doom-unicode-font ()
-      doom-big-font (font-spec :family "iosevka term ss08" :size 22))
+;; don't show a confirmation dialog on closing
+(setq confirm-kill-emacs nil)
 
-(setq default-text-properties '(line-spacing 0.1 line-height 1.1))
+;; start emacs maximized
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; just integrate system clipboard with emacs already https://www.reddit.com/r/emacs/comments/l46om0/utilise_wsl_clipboard_in_doom_emacs/gknmko6/
+(setq save-interprogram-paste-before-kill t)
+
+(add-hook! typescript-tsx-mode 'turn-on-evil-matchit-mode)
 
 ;; TypeScript LSP creates log files directly into project dir, this removes that
 (setenv "TSSERVER_LOG_FILE" "/tmp/tsserver.log")
@@ -96,47 +106,34 @@
 (map! :i "M-o" #'evil-open-below)
 (map! :i "M-O" #'evil-open-above)
 
-;; don't show a confirmation dialog on closing
-(setq confirm-kill-emacs nil)
+;; Switch to the new window after splitting. Taken from https://github.com/sagittaros/doom.d/blob/main/%2Beditor.el
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
 
-;; vim like scrolloff
+;; vim like scrolloff. More at https://www.reddit.com/r/emacs/comments/4hpjwp/vim_like_scrolling_in_emacs/d2rh8g4/
 (setq scroll-margin 2)
-;; there's more at https://www.reddit.com/r/emacs/comments/4hpjwp/vim_like_scrolling_in_emacs/d2rh8g4/
 
-;; h and l to move up/down the directory
 (map! :map dired-mode-map
       :n "h" 'dired-up-directory
       :n "l" 'dired-find-file
       :n "M-l" 'dired-display-file
       :n "C-k" 'dired-do-kill-lines
-      ;; by default it is bound to epa, https://www.gnu.org/software/emacs/manual/html_mono/epa.html
-      ;; i don't use it
+      ;; by default it is bound to epa, https://www.gnu.org/software/emacs/manual/html_mono/epa.html,
+      ;; but i don't use it
       :nv ";" 'evil-ex)
 
 ;; if two dired for dir d1 and d2 are open,
 ;; upon copy file from d1, dired would by default suggest to put it into d2
 (setq dired-dwim-target t)
 
-(map! :leader :n "e" #'flycheck-explain-error-at-point
-      :leader :n "E" #'flycheck-buffer)
-(map! :m "[e" #'flycheck-previous-error
-      :m "]e" #'flycheck-next-error)
-
-;; start emacs maximized
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-;; just integrate system clipboard with emacs already https://www.reddit.com/r/emacs/comments/l46om0/utilise_wsl_clipboard_in_doom_emacs/gknmko6/
-(setq save-interprogram-paste-before-kill t)
+;; M-<numeral> to insert from completion
+(setq company-show-quick-access t)
 
 ;; Projectile, you do a terrible job of caching file names. I'll manually enable it when I'd need to.
 ;; If you want to enable caching back, see this first: https://emacs.stackexchange.com/questions/2164/projectile-does-not-show-all-files-in-project
 ;; Use SPC p i (projectile-invalidate-cache) if you're still seeing files that the project currently doesn't has.
 (setq projectile-enable-caching nil)
-;; also see https://docs.projectile.mx/projectile/configuration.html#project-indexing-method
-
-;; Switch to the new window after splitting. Taken from https://github.com/sagittaros/doom.d/blob/main/%2Beditor.el
-(setq evil-split-window-below t
-      evil-vsplit-window-right t)
+;; Also see https://docs.projectile.mx/projectile/configuration.html#project-indexing-method
 
  ;;  josh
  ;;  sam
@@ -144,7 +141,7 @@
  ;;  C.J.
  ;;  toby
  ;;
- ;; entering a quote is optional
+ ;; Entering a quote is optional
 (defun arrayify (start end quote)
   "Turn strings on newlines into a QUOTEd, comma-separated one-liner."
   (interactive "r\nMQuote: ")
@@ -157,10 +154,10 @@
 
 (map! :leader :nv "r" #'evil-replace-with-register)
 
-;; M-<numeral> to insert from completion
-(setq company-show-quick-access t)
-
-(add-hook! typescript-tsx-mode 'turn-on-evil-matchit-mode)
+(map! :leader :n "e" #'flycheck-explain-error-at-point
+      :leader :n "E" #'flycheck-buffer)
+(map! :m "[e" #'flycheck-previous-error
+      :m "]e" #'flycheck-next-error)
 
 ;; taken from https://gist.github.com/Blaisorblade/c7349438b06e7b1e034db775408ac4cb
 ;; // BEG flycheck
