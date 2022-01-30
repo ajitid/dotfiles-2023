eb36f5fec70543a3d628d44d7f13705ff80ed96b
ajit <zlksnkwork@gmail.com>
2022-01-30 14:18:08 +0530

    org timer

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -42,6 +42,12 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; for M-x org-timer-set-timer
+;; shorthand: 0:3 for sec and 23 for mins
+;; more on this comment https://www.youtube.com/watch?v=JbHE819kVGQ&lc=UgyZXlkdtn31Y8elk8V4AaABAg.9QYI1Vgfhg59QYJARNFmZx
+(setq org-clock-sound "~/scripts/bell.wav")
+(setq org-show-notification-handler 'message)
+
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
