ff0c78f3e90793621287a0dd3ae715e8d9c0dc81
ajit <zlksnkwork@gmail.com>
2022-02-09 11:07:23 +0530

    welp

--- nvim/.config/nvim/mine/lsp.lua
+++ nvim/.config/nvim/mine/lsp.lua
@@ -17,6 +17,64 @@ require'lspconfig'.gopls.setup{
   end,
 }
 
+local lsp_installer = require("nvim-lsp-installer")
+
+-- from https://github.com/williamboman/nvim-lsp-installer/blob/b14bd0c5d75ca9da91d7675e98b89450b08f0143/lua/nvim-lsp-installer/extras/tsserver.lua
+-- also see https://github.com/jose-elias-alvarez/nvim-lsp-ts-utils
+function send_client_request(client_name, ...)
+    for _, client in pairs(vim.lsp.get_active_clients()) do
+        if client.name == client_name then
+            client.request(...)
+        end
+    end
+end
+
+function typescript_rename_file(old, new)
+    local old_uri = vim.uri_from_fname(old)
+    local new_uri = vim.uri_from_fname(new)
+
+    send_client_request("tsserver", "workspace/executeCommand", {
+        command = "_typescript.applyRenameFile",
+        arguments = {
+            {
+                sourceUri = old_uri,
+                targetUri = new_uri,
+            },
+        },
+    })
+end
+
+function rename_file_command()
+  local old_name = vim.fn.expand('%:p:.')
+  local new_name = vim.fn.input("File: ", old_name, "file")
+  typescript_rename_file(old_name, new_name)
+end
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+      capabilities = capabilities,
+      on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+        vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, {buffer=0})
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer=0})
+        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {buffer=0})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
+        vim.keymap.set("n", "<leader>cR", rename_file_command, {buffer=0})
+
+        vim.keymap.set("n", "<leader>df", "<cmd>Telescope diagnostics<cr>", {buffer=0})
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {buffer=0})
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {buffer=0})
+      end,
+    }
+
+    if server.name == "tsserver" then
+    end
+
+    server:setup(opts)
+end)
+
 local cmp = require'cmp'
 
 cmp.setup({
