317fa91e41f846552c913acad67de5d3c38cbffa
ajit <zlksnkwork@gmail.com>
2022-02-13 15:31:58 +0530

    refactor

--- nvim/.config/nvim/mine/treesitter-and-comment.lua
+++ nvim/.config/nvim/mine/treesitter-and-comment.lua
@@ -29,16 +29,10 @@ ts.setup {
 }
 
 require('Comment').setup({
-  ---@param ctx Ctx
   pre_hook = function(ctx)
-    -- Only calculate commentstring for tsx filetypes
     if vim.bo.filetype == 'typescriptreact' then
       local U = require('Comment.utils')
 
-      -- Detemine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-      -- Determine the location where to calculate commentstring from
       local location = nil
       if ctx.ctype == U.ctype.block then
         location = require('ts_context_commentstring.utils').get_cursor_location()
@@ -46,10 +40,10 @@ require('Comment').setup({
         location = require('ts_context_commentstring.utils').get_visual_start_location()
       end
 
-      return require('ts_context_commentstring.internal').calculate_commentstring({
-        key = type,
+      return require('ts_context_commentstring.internal').calculate_commentstring {
+        key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
         location = location,
-      })
+      }
     end
   end,
 })
