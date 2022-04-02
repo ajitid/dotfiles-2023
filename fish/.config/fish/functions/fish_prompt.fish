9e0919a6e140d0180fa563e0b0959ff27e67d92c
ajit <zlksnkwork@gmail.com>
2022-04-02 18:35:34 +0530

    show last program status code

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,14 +1,15 @@
 # Defined interactively
 function fish_prompt
-    set -l __last_command_exit_status $status
+    set -l exit_status $status
 
     set -l cyan (set_color -o cyan)
     set -l red (set_color -o red)
     set -l green (set_color -o green)
     set -l normal (set_color normal)
+    set -l italics (set_color -i)
 
     set -l arrow_color "$green"
-    if test $__last_command_exit_status != 0
+    if test $exit_status != 0
         set arrow_color "$red"
     end
 
@@ -19,6 +20,9 @@ function fish_prompt
 
     set -l cwd $cyan(basename (prompt_pwd))
 
+    if test $exit_status != 0
+        echo "$italics<exited with" $exit_status "status>$normal"
+    end
     echo -s $cwd
     echo -s $arrow $normal
 end
