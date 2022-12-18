c05ccd63f5e12e14626c091fd597c48fddc116d4
Ajit <zlksnkwork@gmail.com>
2022-12-19 03:26:38 +0530

    pnpm autocomplete

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -327,3 +327,10 @@ set -x EDITOR "nvim.appimage"
 set -gx PNPM_HOME "/home/ajitid/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
+
+abbr pn "pnpm"
+
+## ajit: this is for pnpm autocompletion https://pnpm.io/completion
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
