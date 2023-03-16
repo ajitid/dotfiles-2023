4416261c72753221e1c5b6a14301b5a633169144
Ajit <zlksnkwork@gmail.com>
2023-03-17 00:54:00 +0530

    gopls cross platform

--- nvim/.config/nvim/lua/mine/lsp/lsp-and-completion.lua
+++ nvim/.config/nvim/lua/mine/lsp/lsp-and-completion.lua
@@ -178,6 +178,14 @@ lspconfig.gopls.setup{
     common_on_attach(client, bufnr)
     require('folding').on_attach()
   end,
+  settings = {
+    gopls = {
+      env = {
+        -- from https://github.com/golang/go/issues/29202#issuecomment-1233042513
+        GOFLAGS = "-tags=windows,linux,darwin"
+      }
+    }
+  }
 }
 
 -- from https://github.com/williamboman/nvim-lsp-installer/blob/b14bd0c5d75ca9da91d7675e98b89450b08f0143/lua/nvim-lsp-installer/extras/tsserver.lua
